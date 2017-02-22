20cf9ddeaabf1349a1dd03c208c7bbb600c9b6d1
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2017-02-22 15:42:49 +0530

    more samples

--- src/driver.py
+++ src/driver.py
@@ -69,6 +69,30 @@ def n3_relu_regularization():
         network3.SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
     net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
 
+def n3_dropout_and_expanded_data():
+    #we have applied dropout to only fully connected layers and not the convolutional layers, as those layers handle the
+    #overfitting better
+    training_data, validation_data, test_data = network3.load_data_shared()
+    expanded_training_data, _, _ = network3.load_data_shared("../data/mnist_expanded.pkl.gz")
+    mini_batch_size = 10
+    net = network3.Network([
+        network3.ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
+                      filter_shape=(20, 1, 5, 5),
+                      poolsize=(2, 2),
+                      activation_fn=network3.ReLU),
+        network3.ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                      filter_shape=(40, 20, 5, 5),
+                      poolsize=(2, 2),
+                      activation_fn=network3.ReLU),
+        network3.FullyConnectedLayer(
+            n_in=40 * 4 * 4, n_out=1000, activation_fn=network3.ReLU, p_dropout=0.5),
+        network3.FullyConnectedLayer(
+            n_in=1000, n_out=1000, activation_fn=network3.ReLU, p_dropout=0.5),
+        network3.SoftmaxLayer(n_in=1000, n_out=10, p_dropout=0.5)],
+        mini_batch_size)
+    net.SGD(expanded_training_data, 40, mini_batch_size, 0.03,
+                 validation_data, test_data)
+
 def timedRun(methodToRun):
     start = time.time()
     result = methodToRun()
